b522508cae8da1c785fc91369270a79074029ec1
terecodes <ukgracetory@gmail.com>
2022-06-25 02:28:30 +0100

    linked a file

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = 'Blessing Ukanah';
+let height = '5ft 9inches';
+let country = 'Nigeria';
+
+alert(name +"\n"+ height +"\n"+ country);
+//OR
+console.log(name);
+console.log(height);
+console.log(country);
